e760e8c704c8ef406a3fc1f2ffeea147de91594a
Ariel Sandor <arielsandor>
2021-08-19 14:37:35 -0300

    revert back commits

--- src/main.rs
+++ src/main.rs
@@ -32,27 +32,6 @@ fn main() {
             .required(true)
             .takes_value(true)
         )
-        .arg(Arg::with_name("userpropfile")
-            .short("u")
-            .long("userpropfile")
-            .help("SQLite userprop file (userprop.db)")
-            .required(true)
-            .takes_value(true)
-        )
-        .arg(Arg::with_name("ldapfile")
-            .short("l")
-            .long("ldapfile")
-            .help("SQLite LDAP file (ldap.db)")
-            .required(true)
-            .takes_value(true)
-        )
-        .arg(Arg::with_name("geofile")
-            .short("g")
-            .long("geofile")
-            .help("GeoLite2 City file (GeoLite2-City.mmdb)")
-            .required(true)
-            .takes_value(true)
-        )
         .arg(Arg::with_name("port")
             .short("p")
             .long("port")
@@ -71,6 +50,8 @@ fn main() {
         )
         .get_matches();
 
+    //let ovpn_log = flags.value_of("file").unwrap();
+    //let ovpn_log = format!("{}", flags.value_of("file").unwrap())[..];
     let expose_port = flags.value_of("port").unwrap();
     let expose_host = flags.value_of("host").unwrap();
 
@@ -79,49 +60,28 @@ fn main() {
     Builder::from_env(Env::default().default_filter_or("info")).init();
 
     info!("Using file: {}", flags.value_of("file").unwrap());
-    info!("Using userpropfile: {}", flags.value_of("userpropfile").unwrap());
-    info!("Using ldapfile: {}", flags.value_of("ldapfile").unwrap());
-    info!("Using geofile: {}", flags.value_of("geofile").unwrap());
 
     // Parse address used to bind exporter to.
     let addr_raw = expose_host.to_owned() + ":" + expose_port;
     let addr: SocketAddr = addr_raw.parse().expect("can not parse listen addr");
 
-    // Need to clone before moving to closure. 
-    // Ref.: https://stackoverflow.com/a/67697017/1003873
-    let flags = flags.clone();
-
     let new_service = move || {
       let ovpn_log = flags.value_of("file").unwrap();
-      let ovpn_ldap = flags.value_of("ldapfile").unwrap();
-      let ovpn_userprop = flags.value_of("userpropfile").unwrap();
 
       let encoder = TextEncoder::new();
       let connection = sqlite::open(&ovpn_log).unwrap();
-      let connection_ldap = sqlite::open(&ovpn_ldap).unwrap();
-
-      // Attaching userprop database to allow join between logs and user properties
-      let attach_command = format!("ATTACH '{}' AS userpropdb;", &ovpn_userprop);
-      connection.execute(&attach_command).unwrap();
-
-      // Need to clone before moving to closure. 
-      // Ref.: https://stackoverflow.com/a/67697017/1003873
-      let flags = flags.clone();
 
       service_fn_ok(move |_request| {
-        let ovpn_geo = flags.value_of("geofile").unwrap();
 
         metrics::ACCESS_COUNTER.inc();
-        let georeader =  maxminddb::Reader::open_readfile(&ovpn_geo).unwrap();
-        
-        // Assumes session older than 24 hours are not active anymore (since active flag is not always updated properly)
+        let georeader =  maxminddb::Reader::open_readfile("/usr/share/geoip/GeoLite2-City.mmdb").unwrap();
+
         let mut statement = connection
-            .prepare("SELECT l.session_id, l.node, l.username, l.common_name, l.real_ip, l.vpn_ip, l.duration, l.bytes_in, l.bytes_out, l.timestamp, (SELECT c.value FROM userpropdb.profile p, userpropdb.config c WHERE c.profile_id = p.id AND c.name = 'crowdstrike_installed' AND lower(p.name) = lower(l.username)), (SELECT strftime('%s', c.value) FROM userpropdb.profile p, userpropdb.config c WHERE c.profile_id = p.id AND c.name = 'crowdstrike_last_seen' AND lower(p.name) = lower(l.username)), (SELECT strftime('%s', c.value) FROM userpropdb.profile p, userpropdb.config c WHERE c.profile_id = p.id AND c.name = 'crowdstrike_last_check' AND lower(p.name) = lower(l.username)), (SELECT c.value FROM userpropdb.profile p, userpropdb.config c WHERE c.profile_id = p.id AND c.name = 'pvt_hw_addr' AND lower(p.name) = lower(l.username)) FROM log l WHERE l.active = 1 and l.auth = 1 and l.start_time >= strftime('%s', datetime('now','-1 days'))")
+            .prepare("SELECT session_id, node, username, common_name, real_ip, vpn_ip, duration, bytes_in, bytes_out, timestamp FROM log WHERE active = 1 and auth = 1 and start_time >= strftime('%s', datetime('now','-1 days'))")
             .unwrap();
         while let State::Row = statement.next().unwrap() {
           let ip: IpAddr = statement.read::<String>(4).unwrap().parse().unwrap();
           
-          // Find location associated with IP
           let city: std::result::Result<Option<geoip2::City>, maxminddb::MaxMindDBError> = georeader.lookup(ip);
           let (c_name, lat, lon) = match city {
             Ok(Some(city)) => (
@@ -135,57 +95,28 @@ fn main() {
           };
 
           let timestamp_ms = statement.read::<i64>(9).unwrap() * 1000;
-          let crowdstrike_last_seen_timestamp_ms = statement.read::<i64>(11).unwrap() * 1000;
-          let crowdstrike_last_check_timestamp_ms = statement.read::<i64>(12).unwrap() * 1000;
-          let username = statement.read::<String>(2);
-
-          // Find user full name from ldap database
-          let mut statement_ldap = connection_ldap
-              .prepare("SELECT cn from users where upper(uid) = upper(?)")
-              .unwrap();
-
-          statement_ldap.bind(1, &username.as_ref().unwrap()[..]).unwrap();
-
-          let mut fullname = "Unknown".to_string();
-          while let State::Row = statement_ldap.next().unwrap() {
-            fullname = statement_ldap.read::<String>(0).unwrap();
-          }
 
           let label_values = [
-            &statement.read::<String>(0).unwrap()[..], 
+            &statement.read::<String>(0).unwrap()[..],
             &statement.read::<String>(1).unwrap()[..],
             &statement.read::<String>(2).unwrap()[..],
-            &fullname,
+            &statement.read::<String>(3).unwrap()[..],
             &statement.read::<String>(4).unwrap()[..],
             &statement.read::<String>(5).unwrap()[..],
             &c_name.unwrap_or("None".to_string()),
             &lat.to_string(),
-            &lon.to_string(),
-<<<<<<< HEAD
-            &statement.read::<String>(13).unwrap_or("".to_string())[..]
-=======
-            &timestamp_ms.to_string()
->>>>>>> parent of b7c6f85 (added record timestamp metrics)
+            &lon.to_string()
           ];
 
           metrics::DURATION.with_label_values(&label_values).set(statement.read::<f64>(6).unwrap());
           metrics::BYTES_IN.with_label_values(&label_values).set(statement.read::<f64>(7).unwrap());
           metrics::BYTES_OUT.with_label_values(&label_values).set(statement.read::<f64>(8).unwrap());
-
-          metrics::CROWDSTRIKE_INSTALLED.with_label_values(&label_values).set(statement.read::<f64>(10).unwrap_or(-1.0));
-          metrics::CROWDSTRIKE_LAST_SEEN_TIMESTAMP.with_label_values(&label_values).set(crowdstrike_last_seen_timestamp_ms as f64);
-          metrics::CROWDSTRIKE_LAST_CHECK_TIMESTAMP.with_label_values(&label_values).set(crowdstrike_last_check_timestamp_ms as f64);
+          metrics::RECORD_TIMESTAMP.with_label_values(&label_values).set(timestamp_ms as f64);
 
           metrics::DURATION.with_label_values(&label_values);
           metrics::BYTES_IN.with_label_values(&label_values);
           metrics::BYTES_OUT.with_label_values(&label_values);
-<<<<<<< HEAD
           metrics::RECORD_TIMESTAMP.with_label_values(&label_values);
-          metrics::CROWDSTRIKE_INSTALLED.with_label_values(&label_values);
-          metrics::CROWDSTRIKE_LAST_SEEN_TIMESTAMP.with_label_values(&label_values);
-          metrics::CROWDSTRIKE_LAST_CHECK_TIMESTAMP.with_label_values(&label_values);
-=======
->>>>>>> parent of b7c6f85 (added record timestamp metrics)
         }
 
         // Gather the metrics.
